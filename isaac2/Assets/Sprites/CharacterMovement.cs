3ac3f0a2b0200471a7e42163ebecf525571d3d07
thatperson24 <pengdis24@gmail.com>
2024-07-13 22:40:07 -0700

    Added basic character x/y movement

---
+++ isaac2/Assets/Sprites/CharacterMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private float horizontal;
+    [SerializeField] private float speed;
+    private bool isFacingRight = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private GameObject character;
+    void Update()
+    {
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        //Horizontal movement
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-1f * speed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        //Vertical movement
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, speed);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, -1f * speed);
+        }
+        else
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+        }
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = character.transform.localScale;
+            localScale.x *= -1f;
+            character.transform.localScale = localScale;
+        }
+    }
+}
