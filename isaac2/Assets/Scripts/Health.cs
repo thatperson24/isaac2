32d45bd2980848f7f87a5c4669883ee4ae771f43
alyssajcrawford1 <alyssajcrawford1@gmail.com>
2024-07-25 18:54:33 -0500

    Increment/set maxHealth, more checks

--- isaac2/Assets/Scripts/Health.cs
+++ isaac2/Assets/Scripts/Health.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
     /// <summary>
-    /// Just stores health. 
+    /// Health class stores entity health and deals with setting, incrementing,
+    /// and fetching both current and max health.
+    /// Also detects death and overheal.
+    /// Used by other scripts.
     /// </summary>
     [SerializeField] private int maxHealth;  // or float?
     private int health;
@@ -19,43 +23,148 @@ public class Health : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Could implement gradual healing over time I guess?
+        // Could implement gradual natural healing over time I guess?
     }
 
+
+
     /// <summary>
-    /// 
+    /// IDK C# DOCUMENTATION CONVENTIONS HELP!!!!!!!!!
+    /// Returns current health of entity.
     /// </summary>
     /// <returns></returns>
-    public int getHealth()
+    public int GetHealth()
     {
         return health;
     }
 
     /// <summary>
-    /// 
+    /// Returns current maxHealth of entity.
     /// </summary>
-    /// <param name="amount"></param>
-    public void modifyHealth(int amount = 1)
+    /// <returns></returns>
+    public int GetMaxHealth()
     {
-        health += amount;
+        return maxHealth;
     }
 
     /// <summary>
-    /// 
+    /// Set current health to a new health value.
+    /// Prevents health modification to already dead entities.
+    /// Detects and prevents overheal.
+    /// Detects death and prevents negative health.
+    /// Returns new health in case caller needs (detect death etc.).
     /// </summary>
     /// <param name="amount"></param>
-    public void damage(int amount = 1)
+    public int SetHealth(int newHealth) 
     {
-        modifyHealth(-amount); 
+        if (health == 0) // ALREADY DEAD
+        {
+            return health;  // prevent healing of dead entities
+        }
+        else if (newHealth > maxHealth) // OVERHEAL
+        {
+            health = maxHealth;
+            // overheal - could block use of heal item or convert heal to something else
+        }
+        else if (newHealth <= 0) // YOU DIED
+        {
+            health = 0;
+            // initiate death stuff - 
+            // obviously differs between players and enemies so might need to
+            // have separate health scripts for each to account for that?
+            // enemies - either despawn or just chill as a corpse
+            // player - uhhhhhhhhhhhhhhhh
+        }
+        else
+        {
+            health = newHealth;
+        }
+        return health;
+    }
+
+    /// <summary>
+    /// Increment current health by given + or - delta amount.
+    /// Calls setHealth which handles death, overheal, etc.
+    /// Delta is 1 by default (enemy heals by 1 HP).
+    /// Returns new health in case caller needs (detect death, etc.).
+    /// </summary>
+    /// <param name="delta"></param>
+    /// <returns></returns>
+    public int IncrementHealth(int delta = 1)
+    {
+        return SetHealth(health + delta);
+    }
+
+    /// <summary>
+    /// Decrement current health (damage) by given delta amount.
+    /// Delta is 1 by default (entity takes 1 HP damage).
+    /// Returns new health in case caller needs (detect death, etc.).
+    /// </summary>
+    /// <param name="delta"></param>
+    /// <returns></returns>
+    public int Damage(int delta = 1)
+    {
+        return IncrementHealth(-delta); 
         // convention question - damage param negative or positive?
+        // visual cues?
     }
 
     /// <summary>
-    /// 
+    /// Increment current health (heal) by given delta amount.
+    /// Delta is 1 by default (entity heals 1 HP).
+    /// Returns new health in case caller needs.
+    /// Might want to return overheal flag???
     /// </summary>
-    /// <param name="amount"></param>
-    public void heal(int amount = 1)
+    /// <param name="delta"></param>
+    /// <returns></returns>
+    public int Heal(int delta = 1)
     {
-        modifyHealth(amount);
+        return IncrementHealth(delta);
+        // visual cues?
+    }
+
+    /// <summary>
+    /// Set current health to the max health / fully heal entity.
+    /// Returns current maxHealth (probably unnecessary).
+    /// </summary>
+    /// <returns></returns>
+    public int FullHeal() // or resetHealth() ??
+    {
+        return SetHealth(maxHealth);
+    }
+
+    /// <summary>
+    /// Set max health to given amount.
+    /// Could be used for powerups etc.
+    /// Returns maxHealth in case caller needs.
+    /// </summary>
+    /// <param name="newMaxHealth"></param>
+    /// <returns></returns>
+    public int SetMaxHealth(int newMaxHealth) 
+    {
+        if (newMaxHealth <= 0)  // THATS ILLEGAL
+        {
+            // ERROR!! resist change (or maybe set to 1?)
+            // TODO: throw error?
+            return maxHealth;
+        }
+        // TODO: maybe put max constraints on maxHealth
+        
+        maxHealth = newMaxHealth;
+        return maxHealth;
+    }
+
+    /// <summary>
+    /// Increment maxHealth by given delta amount.
+    /// Delta is 1 by default (entity gets 1 more max HP).
+    /// Could be used for powerups etc.
+    /// Returns new maxHealth in case caller needs.
+    /// </summary>
+    /// <param name="delta"></param>
+    /// <returns></returns>
+    public int IncrementMaxHealth(int delta = 1) 
+    {
+        // TODO: deal with constraints / error checking
+        return SetMaxHealth(maxHealth + delta);
     }
 }
