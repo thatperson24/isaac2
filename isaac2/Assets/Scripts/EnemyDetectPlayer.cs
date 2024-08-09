9fe338aad71b27f908efc7e4592c769e6398df2f
alyssajcrawford1 <alyssajcrawford1@gmail.com>
2024-08-08 19:40:44 -0500

    Draw Gizmos for in-editor viz

--- isaac2/Assets/Scripts/EnemyDetectPlayer.cs
+++ isaac2/Assets/Scripts/EnemyDetectPlayer.cs
@@ -23,6 +23,7 @@ public class EnemyDetectPlayer : MonoBehaviour
     /// </summary>
     
     private GameObject player;
+    private int playerLayer = 8;
     
     [SerializeField] private bool isAlert;
     private bool isDead;
@@ -77,37 +78,6 @@ public class EnemyDetectPlayer : MonoBehaviour
     }
 
     /// <summary>
-    ///     Determines if Enemy can see Player (or Bullet).
-    ///     Draws sector-shaped FOV with the set angle and radius,
-    ///     and detects if Player is within that FOV but not obscured.
-    /// </summary>
-    private void EnemySight()
-    {
-        int playerLayer = 8;
-        int layerMask = 1 << playerLayer;
-        Collider2D[] FOV = Physics2D.OverlapCircleAll(this.transform.position, sightRadius, layerMask);  // draw radius on Player layer (8)
-
-        if(FOV.Length > 0)  // if any entity (Player or Bullet) detected in radius
-        {
-            Collider2D playerCollider = FOV[0];
-            Vector2 directionToPlayer = (playerCollider.transform.position - this.transform.position).normalized;
-
-            if (Vector2.Angle(this.transform.right, directionToPlayer) < sightAngle * 0.5)  // if Player/Bullet in FOV angle
-            {
-                float distanceToPlayer = Vector2.Distance(this.transform.position, playerCollider.transform.position);
-                
-                // isAlert = true if Player/Bullet not obstructed from view
-                // Draw ray from Enemy to Player/Bullet, detect if any collisions (exclude Player layer 8)
-                if (Physics2D.Raycast(this.transform.position, directionToPlayer, distanceToPlayer, ~layerMask))
-                {
-                    isAlert = true;
-                }
-            }
-        }
-        // TODO: Detect when Player's Flashlight cast within FOV?
-    }
-
-    /// <summary>
     ///     Determines if Enemy can "hear" Player.
     ///     Currently just detects if Player within hearing radius.
     ///     Ideally would detect Player actions that make noise within that radius.
@@ -121,6 +91,90 @@ public class EnemyDetectPlayer : MonoBehaviour
         // TODO: detect when Player interacts with objects in this radius
         // TODO: detect when Player shoots Bullet in this radius
     }
+
+    /// <summary>
+    ///     Determines if Enemy can see Player (or Bullet).
+    ///     Draws sector-shaped FOV with the set angle and radius,
+    ///     and detects if Player is within that FOV but not obscured.
+    ///     By default, facing straight upwards, but rotates with
+    ///     Enemy on z-axis.
+    /// </summary>
+    private void EnemySight()
+    {
+        int layerMask = 1 << playerLayer;
+        Collider2D[] FOV = Physics2D.OverlapCircleAll(this.transform.position, sightRadius, layerMask);  // draw radius on Player layer (8)
+
+        if(FOV.Length > 0)  // if any entity (Player or Bullet) detected in radius
+        {
+            Collider2D targetCollider = FOV[0];
+            Vector2 targetDirection = (targetCollider.transform.position - this.transform.position).normalized;
+
+            if (Vector2.Angle(this.transform.up, targetDirection) < sightAngle * 0.5)  // if Player/Bullet in FOV angle
+            {
+                float distanceFromTarget = Vector2.Distance(this.transform.position, targetCollider.transform.position);
+                
+                // isAlert = true if Player/Bullet not obstructed from view
+                // Draw ray from Enemy to Player/Bullet, detect if any collisions (exclude Player layer 8)
+                if (Physics2D.Raycast(this.transform.position, targetDirection, distanceFromTarget, ~layerMask))
+                {
+                    isAlert = true;
+                }
+            }
+        }
+        // TODO: Detect when Player's Flashlight cast within FOV?
+    }
+
+    /// <summary>
+    ///     Draws FOV and hearing visualizions around Enemy in Scene Editor.
+    ///     Only draws when Enemy is Idle/non-Alert.
+    /// </summary>
+    private void OnDrawGizmos()
+    {
+        // Draw Enemy FOV when still Idle
+        if (!isAlert)
+        {
+            DrawSightGizmo();
+            DrawHearingGizmo();
+        }
+    }
+
+    /// <summary>
+    ///     Draws "solid arc" AKA circle sector in front of Enemy that visualizes their
+    ///     sight radius & angle / FOV, viewable in Scene editor.
+    /// </summary>
+    private void DrawSightGizmo()
+    {
+        if (sightAngle != 0 && sightRadius >= 0)  // don't do if blind
+        {
+            Vector3 start = new Vector3(
+                Mathf.Sin(((sightAngle * 0.5f) - transform.eulerAngles.z) * Mathf.Deg2Rad),
+                Mathf.Cos(((sightAngle * 0.5f) - transform.eulerAngles.z) * Mathf.Deg2Rad));
+
+            Handles.color = new Color(255, 0, 0, 0.03f);
+            Handles.DrawSolidArc(transform.position, Vector3.forward, start, sightAngle, sightRadius);
+        }
+    }
+
+    /// <summary>
+    ///     Draws circle around Enemy that visualizes their
+    ///     hearing radius, viewable in Scene editor.
+    /// </summary>
+    private void DrawHearingGizmo()
+    {
+        if (hearingRadius >= 0)  // don't do if deaf
+        {
+            Handles.color = Color.yellow;
+            UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.forward, hearingRadius);
+        }
+    }
+
+    // ***************************************
+    // I have no previous C# experience
+    // I just learned about Properties ...
+    // Should I do that instead???
+    // I think the answer is yes??
+    // ***************************************
+
 
     /// <summary>
     ///     Return whether or not the current enemy is Alert
@@ -208,7 +262,4 @@ public class EnemyDetectPlayer : MonoBehaviour
     {
         sightAngle = newSightAngle;
     }
-    
-
-
 }
